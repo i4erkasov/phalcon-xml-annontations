0213ca4e60681846fa7bdebcc54159bf31150651
ivan cherkasov <ivan.cherkasov@quadcode.com>
2023-06-01 00:59:11 +0300

    add tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+require_once dirname(__FILE__) . '/../src/Annotations/AbstractAttribute.php';
+require_once dirname(__FILE__) . '/../src/Annotations/AbstractTag.php';
+require_once dirname(__FILE__) . '/../src/Annotations/Document.php';
+require_once dirname(__FILE__) . '/../src/Annotations/Tag.php';
+require_once dirname(__FILE__) . '/../src/Annotations/Attribute.php';
+require_once dirname(__FILE__) . '/../src/Interfaces/AnalyzerInterface.php';
+require_once dirname(__FILE__) . '/../src/Interfaces/XmlWriterInterface.php';
+require_once dirname(__FILE__) . '/../src/Analyzer.php';
+require_once dirname(__FILE__) . '/../src/XmlAnnotations.php';
+require_once dirname(__FILE__) . '/../src/XmlWriter.php';
